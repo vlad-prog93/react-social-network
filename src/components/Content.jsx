1f48627e46976e903efbdfa6d1995f61e5f108a0
Doroshenko Vladislav <vladka_@bk.ru>
2022-03-24 16:36:20 +0300

    continue

---
+++ src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Content.css';
+
+const Content = () => {
+  return (
+    <div className="content">
+      <img src="https://picwalls.ru/img/gallery/27/thumbs/thumb_l_3366pw.jpg" />
+    </div>
+  )
+}
+
+export default Content
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className="header">
+      <img src="https://img.icons8.com/metro/208/name.png" alt="logo"/>
+    </div>
+  )
+}
+
+export default Header
